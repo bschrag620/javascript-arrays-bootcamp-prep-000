ee2d29b61da1fcaf0773e7c0e1061b959df0c6cc
Brad Schrag <brad.schrag@gmail.com>
2018-06-23 05:59:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,19 @@
+var chocolateBars = Array [
+  "snickers", 
+  "hundred grand", 
+  "kitkat",
+  "skittles"]
+
 function addElementToBeginningOfArray(array, element) {
-  return [...array, element]
+  return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element)
   return array
 }
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
